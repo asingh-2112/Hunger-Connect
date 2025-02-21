ca2e4f101236367ace877d127c168bdf19ed0718
asingh-2112 <aksinghrbj2112@gmail.com>
2025-02-21 15:19:37 +0530

    Created Component Folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Button } from '@material-tailwind/react'
 import React from 'react'
 
-function App() {
+function Home() {
   return (
-    <div>
+    <div className='flex justify-center items-center h-screen'>
       <Button>Click</Button>
-    </div> 
+    </div>
   )
 }
 
-export default App
+export default Home
