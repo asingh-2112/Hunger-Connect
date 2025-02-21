a1cc487998148a13af2f653c54a93f0c813f0ee8
asingh-2112 <aksinghrbj2112@gmail.com>
2025-02-21 17:12:07 +0530

    Project Setup Completed: routes created, context api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import { Button } from '@material-tailwind/react'
 import React from 'react'
+import { Route, Router, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import AllBlogs from './pages/allBlogs/AllBlogs'
+import Blog from './pages/blog/Blog'
+import BlogInfo from './pages/blogInfo/BlogInfo'
+import AdminLogin from './pages/admin/adminLogin/AdminLogin'
+import Dashboard from './pages/admin/dashboard/Dashboard'
+import Nopage from './pages/nopage/Nopage'
+import MyState from './context/data/myState'
+import { Toaster } from 'react-hot-toast'
 
-function Home() {
+function App() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <Button>Click</Button>
+    <div >
+      <MyState>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/blog" element={<Blog/>} />
+            <Route path="/allblogs" element={<AllBlogs/>} />
+            <Route path="/bloginfo/:id" element={<BlogInfo/>} />
+            <Route path="/adminlogin" element={<AdminLogin/>} />
+            <Route path="/dashboard" element={<Dashboard/>} />
+            <Route path="/*" element={<Nopage/>} />
+          </Routes>
+          <Toaster/>
+        </Router>
+      </MyState>
     </div>
   )
 }
 
-export default Home
+export default App
---
+++ src/context/data/myContext.jsx
@@ -0,0 +1,4 @@
+const { createContext } = require("react");
+
+const myContext= createContext();
+export default myContext;
