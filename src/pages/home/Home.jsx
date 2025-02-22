af632d7c7c4d048d8ce460b7e8b38fef08033677
asingh-2112 <aksinghrbj2112@gmail.com>
2025-02-23 00:59:24 +0530

    Blog Post Card Created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../../components/layout/Layout'
 import HeroSection from '../../components/heroSection/HeroSection'
+import BlogPostCard from '../../components/blogPostCard/BlogPostCard'
 
 export default function Home() {
   return (
     <Layout>
       <HeroSection/>
+      <BlogPostCard/>
     </Layout>
   )
 }
