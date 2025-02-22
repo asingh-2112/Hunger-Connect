d07a5fac36c8f575d4308f22e93e404c8965f611
asingh-2112 <aksinghrbj2112@gmail.com>
2025-02-23 00:51:41 +0530

    Hero Section intially initialized

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Layout from '../../components/layout/Layout'
+import HeroSection from '../../components/heroSection/HeroSection'
 
 export default function Home() {
   return (
-    <Layout>Home</Layout>
+    <Layout>
+      <HeroSection/>
+    </Layout>
   )
 }
