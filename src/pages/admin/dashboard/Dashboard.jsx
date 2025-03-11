c2fd228c2348bb0936e76c9bf7c4697207c006dc
asingh-2112 <aksinghrbj2112@gmail.com>
2025-03-11 21:34:47 +0530

    Get Blogs from the DB and displaying it in dashboard

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -6,8 +6,10 @@ import { Link, useNavigate } from 'react-router-dom';
 
 function Dashboard() {
     const context = useContext(myContext);
-    const { mode } = context;
+    const { mode, getAllBlog } = context;
     const navigate=useNavigate();
+
+    console.log(getAllBlog)
 
     const logout=()=>{
         localStorage.clear();
@@ -126,42 +128,52 @@ function Dashboard() {
                                 </thead>
 
                                 {/* tbody  */}
-                                <tbody>
-                                    <tr className=" border-b-2" style={{ background: mode === 'dark' ? 'rgb(30, 41, 59)' : 'white' }}>
-                                        {/* S.No   */}
-                                        <td style={{ color: mode === 'dark' ? 'white' : 'black' }} className="px-6 py-4">
-                                            {'1.'}
-                                        </td>
-
-                                        {/* Blog Thumbnail  */}
-                                        <th style={{ color: mode === 'dark' ? 'white' : 'black' }} scope="row" className="px-6 py-4 font-medium ">
-                                            {/* thumbnail  */}
-                                            <img className='w-16 rounded-lg' src={'https://firebasestorage.googleapis.com/v0/b/blog-fea71.appspot.com/o/blogimage%2FReact%20Introduction.png?alt=media&token=1ba7496b-2cbc-450c-ab1a-57e19882dc76'} alt="thumbnail" />
-                                        </th>
-
-                                        {/* Blog Title  */}
-                                        <td style={{ color: mode === 'dark' ? 'white' : 'black' }} className="px-6 py-4">
-                                            {'React Introduction'}
-                                        </td>
-
-                                        {/* Blog Category  */}
-                                        <td style={{ color: mode === 'dark' ? 'white' : 'black' }} className="px-6 py-4">
-                                            {'reactjs'}
-                                        </td>
-
-                                        {/* Blog Date  */}
-                                        <td style={{ color: mode === 'dark' ? 'white' : 'black' }} className="px-6 py-4">
-                                            {'Jul 25, 2023'}
-                                        </td>
-
-                                        {/* Delete Blog  */}
-                                        <td style={{ color: mode === 'dark' ? 'white' : 'black' }} className="px-6 py-4">
-                                            <button className=' px-4 py-1 rounded-lg text-white font-bold bg-red-500'>
-                                                Delete
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </tbody>
+                                {getAllBlog.length > 0
+                                    ?
+                                    <>
+                                        {getAllBlog.map((item, index) => {
+                                            const {thumbnail, date} = item;
+                                            console.log(item)
+                                            return (
+                                                <tbody>
+                                                    <tr className=" border-b-2" style={{ background: mode === 'dark' ? 'rgb(30, 41, 59)' : 'white' }}>
+                                                        {/* S.No   */}
+                                                        <td style={{ color: mode === 'dark' ? 'white' : 'black' }} className="px-6 py-4">
+                                                            {index + 1}.
+                                                        </td>
+                                                        {/* Blog Thumbnail  */}
+                                                        <th style={{ color: mode === 'dark' ? 'white' : 'black' }} scope="row" className="px-6 py-4 font-medium ">
+                                                            {/* thumbnail  */}
+                                                            <img className='w-16 rounded-lg' 
+                                                            src={thumbnail} alt="thumbnail" />
+                                                        </th>
+                                                        {/* Blog Title  */}
+                                                        <td style={{ color: mode === 'dark' ? 'white' : 'black' }} className="px-6 py-4">
+                                                            {item.blogs.title}
+                                                        </td>
+                                                        {/* Blog Category  */}
+                                                        <td style={{ color: mode === 'dark' ? 'white' : 'black' }} className="px-6 py-4">
+                                                            {item.blogs.category}
+                                                        </td>
+                                                        {/* Blog Date  */}
+                                                        <td style={{ color: mode === 'dark' ? 'white' : 'black' }} className="px-6 py-4">
+                                                            {date}
+                                                        </td>
+                                                        {/* Delete Blog  */}
+                                                        <td style={{ color: mode === 'dark' ? 'white' : 'black' }} className="px-6 py-4">
+                                                            <button className=' px-4 py-1 rounded-lg text-white font-bold bg-red-500'>
+                                                                Delete
+                                                            </button>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            )
+                                        })}</>
+                                    :
+                                    <>
+                                        <h1>Not Found</h1>
+                                    </>
+                                }
                             </table>
                         </div>
                     </div>
