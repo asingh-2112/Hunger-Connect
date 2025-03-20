aaa9bd0eb1490530e3efc6544a119184cc33c160
asingh-2112 <aksinghrbj2112@gmail.com>
2025-03-20 21:09:16 +0530

    added plus button on home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/layout/Layout'
 import HeroSection from '../../components/heroSection/HeroSection'
 import BlogPostCard from '../../components/blogPostCard/BlogPostCard'
+import PlusButton from '../../components/plusButton/PlusButton';
+import CreateDonation from '../../components/createDonation/CreateDonation';
 
 export default function Home() {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const userRole = storedUser?.role;
+  const [open, setOpen] = useState(false);
   useEffect(() => {
     window.scrollTo(0, 0)
 }, [])
@@ -11,6 +16,15 @@ export default function Home() {
     <Layout>
       <HeroSection/>
       <BlogPostCard/>
+      {(userRole==="provider") &&(
+        <>
+        {/* ✅ Plus Button to Open Modal */}
+        <PlusButton onClick={() => setOpen(true)} />
+
+        {/* ✅ Create Donation Modal */}
+        <CreateDonation open={open} setOpen={setOpen} />
+        </>
+      )}
     </Layout>
   )
 }
